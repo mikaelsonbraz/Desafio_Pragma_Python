d9d7a66ab351559f42960f1086805b3984917e9d
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-05-11 11:06:40 -0300

    Parser finished

--- challenge.py
+++ challenge.py
@@ -1,52 +1,62 @@
-
 from functions import *
+import jsonpickle
 
+with open('arquivos/Quake.txt', 'r') as QuakeTxt:
 
-with open('arquivos/gameInfoTest.txt', 'r') as gameInfoTest:
-
-    linhas = []
+    lines = []
     archive_json = []
     game = 0
     total_kills = 0
     players = []
 
-    dict = {"game": game,
-            "status": {
-                "total_kills": total_kills,
-                "players": []
-            }}
+    for row in QuakeTxt.readlines():
+        lines.append(row)
 
-    for row in gameInfoTest.readlines():
-        linhas.append(row)
+    for line in lines:
 
-    for linha in linhas:
-
-        print(linha.split())
-
-        if linha.split()[1].__eq__('InitGame:'):
+        if line.split()[1].__eq__('InitGame:'):
+            dict = {"game": game,
+                    "status": {
+                        "total_kills": total_kills,
+                        "players": players
+                    }}
             game += 1
 
-        elif linha.split()[1].__eq__('ShutdownGame:'):
-            dict["game"] = game
-            dict["status"]["players"] = players
-            archive_json.append(dict)
-            total_kills = 0
-            players = []
-
-        elif linha.split()[1].__eq__('ClientUserinfoChanged:'):
-            passa = True
-
+        elif line.split()[1].__eq__('ClientUserinfoChanged:'):
+            changeName = False
             for item in players:
-                if item["id"] == linha.split()[2]:
-                    passa = False
-                    if item["nome"] == findPlayerName(linha):
+                if item["id"] == line.split()[2]:
+                    changeName = True
+                    if item["nome"] == findPlayerName(line):
                         pass
                     else:
                         item["old_names"].append(item["nome"])
-                        item["nome"] = findPlayerName(linha)
-
-            if passa:
-                player = {"id": linha.split()[2], "nome": findPlayerName(linha), "kills": 0, "old_names": []}
+                        item["nome"] = findPlayerName(line)
+            if not changeName:
+                player = {"id": line.split()[2], "nome": findPlayerName(line), "kills": 0, "old_names": []}
                 players.append(player)
 
-print(archive_json)
+        elif line.split()[1].__eq__('Kill:'):
+            total_kills += 1
+            playerDeath = updateKills(line)
+            for item in players:
+                if item["nome"].__eq__(playerDeath[0]):
+                    item["kills"] += playerDeath[1]
+                    if item["kills"] < 0:
+                        item["kills"] = 0
+
+        elif line.split()[1].__eq__('ShutdownGame:'):
+            dict["game"] = game
+            dict["status"]["total_kills"] = total_kills
+            dict["status"]["players"] = players
+            archive_json.append(dict)
+            players = []
+            total_kills = 0
+
+QuakeTxt.close()
+
+with open('arquivos/Quake.json', 'w') as QuakeJson:
+    json = jsonpickle.encode(archive_json, indent=4)
+    QuakeJson.write(json)
+QuakeJson.close()
+
--- functions.py
+++ functions.py
@@ -1,5 +1,14 @@
-
-
-def findPlayerName(list: str) -> str:
-    nome = list.split('n\\')[1].split('\\t')
+def findPlayerName(line: str) -> str:
+    nome = line.split('n\\')[1].split('\\t')
     return nome[0]
+
+def updateKills(line: str) -> tuple:
+    killData = line.split(':')
+    firstPlayer = killData[3].split('killed')[0].strip()
+    secondPlayer = killData[3].split('killed')[1].split('by')[0].strip()
+    if firstPlayer.__eq__('<world>'):
+        return secondPlayer, -1
+    elif firstPlayer.__eq__(secondPlayer):
+        return secondPlayer, -1
+    else:
+        return firstPlayer, 1
